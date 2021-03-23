f04f4dbe263dfa93250bfd20e174b7ba370eb2c2
Rush-K <k_rush@naver.com>
2021-03-23 23:37:36 +0900

    전화번호 목록 문제 해결

--- Programmers/src/Level2/PhoneBook.java
+++ Programmers/src/Level2/PhoneBook.java
@@ -1,25 +1,31 @@
+/**
+ * Programmers - Hash Category
+ * Problem Name : 전화번호 목록 
+ * Writed by Rush.K
+ */
+
 package Level2;
 
 import java.util.HashMap;
 
 public class PhoneBook {
 
-	public static HashMap<String, Integer> hashMap;
+	public static HashMap<String, Integer> hashMap; // hashMap 선언 
 	
 	public static boolean solution(String[] phone_book) {
 		hashMap = new HashMap<String, Integer>();
 		
-		for (int i = 0; i < phone_book.length; i++) {
+		for (int i = 0; i < phone_book.length; i++) { // ex) "119" -> "1", "11", "119" 를 HashMap에 삽입 
 			for (int j = 1; j <= phone_book[i].length(); j++) {
-				if (hashMap.get(phone_book[i].substring(0, j)) == null) {
+				if (hashMap.get(phone_book[i].substring(0, j)) == null) { // HashMap에 존재하지 않는 값일 경우 
 					hashMap.put(phone_book[i].substring(0, j), 1);
-				} else {
+				} else { // HashMap에 이미 존재하는 경우 
 					hashMap.put(phone_book[i].substring(0, j), hashMap.get(phone_book[i].substring(0, j)) + 1);
 				}
 			}
 		}
 		
-		for (int i = 0; i < phone_book.length; i++) {
+		for (int i = 0; i < phone_book.length; i++) { // Hash 값이 2 이상일 경우 접두어 존재 
 			if (hashMap.get(phone_book[i]) > 1) return false;
 		}
 		
